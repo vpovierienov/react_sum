83efcd6e21f2d45fd326be97b8cd0f63ae2a0304
itpoverenov@gmail.com <itpoverenov@gmail.com>
2026-03-31 01:45:12 +0200

    add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,8 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => {
+  return (
+    <p>
+      Sum of <strong>{a}</strong> and <strong>{b}</strong>
+      is <strong>{a + b}</strong>
+    </p>
+  );
+};
