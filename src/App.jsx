008b69233a72818d4c5ead2421568302b61892e0
itpoverenov@gmail.com <itpoverenov@gmail.com>
2026-03-31 02:42:11 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 import './App.scss';
 import { Sum } from './components/Sum/Sum';
 
-export const App = () => {
-  return (
-    <>
-      <h1>Простой калькулятор суммы на React</h1>
-
-      <Sum a={2} b={3} />
-      <Sum a={-5} b={5} />
-      <Sum a={10} b={0} />
-      <Sum a={0} b={5} />
-      <Sum a={0} b={0} />
-
-      {/* Можно добавить ещё примеры */}
-      <Sum a={42} b={58} />
-    </>
-  );
-};
+export const App = () => (
+  <>
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
+  </>
+);
